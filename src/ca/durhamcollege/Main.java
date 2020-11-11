6edba0b150b2203ef366e53dff01b396281d31ab
home <andreagrippa76@gmail.com>
2020-11-11 15:56:24 -0500

    Input and output test with two variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -3,6 +3,7 @@
  * Date: November 11, 2020
  */
 
+
 package ca.durhamcollege;
 
 import java.util.Scanner;
@@ -12,12 +13,28 @@ public class Main
 
     public Main()
     {
-        String name = "";
+        //define our Scanner
         Scanner keyboard = new Scanner(System.in);
+
+        //Variables
+        String name = "";
+        float age = 0.0f;
+
+        //Input
+
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
 
-        System.out.printf("You entered: %s", name);
+
+
+
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+        System.out.println();
+
+        //Output
+        System.out.printf("You entered: %s \n", name);
+        System.out.printf("You entered: %.2f\n", age);
     }
 
     public static void main(String[] args)
