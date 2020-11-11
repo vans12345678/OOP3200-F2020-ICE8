abc04785ebf8e0282a6aac2d92c932ff47a05bad
home <andreagrippa76@gmail.com>
2020-11-11 15:47:57 -0500

    Included basic input/output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,13 +1,27 @@
+/**
+ * Author: Andre Agrippa
+ * Date: November 11, 2020
+ */
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
 
     public Main()
     {
+        String name = "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
 
+        System.out.printf("You entered: %s", name);
     }
 
-    public static void main(String[] args) {
-	new Main();
+    public static void main(String[] args)
+    {
+	    new Main();
     }
 }
